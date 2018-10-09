804d1f306e2b752f7df5d7d8a2d696a40b32b990
Hayden Allen Johnson <hayden.johnson@mail.utoronto.ca>
2018-10-09 17:13:28 -0400

    changed figure name

--- Lab05/code/Lab05_Q1_a.py
+++ Lab05/code/Lab05_Q1_a.py
@@ -8,3 +8,11 @@ Created on Tue Oct  9 16:25:26 2018
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+x = np.arange(0,100,1)
+
+y = x**2
+
+plt.figure(0)
+plt.plot(x,y)
+plt.savefig('../images/name.png')
