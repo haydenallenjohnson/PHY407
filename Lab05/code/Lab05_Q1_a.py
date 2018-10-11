bac7a5242d02f35c94b6f853f4a1271f2a5559cf
Hayden Allen Johnson <hayden.johnson@mail.utoronto.ca>
2018-10-11 14:16:31 -0400

    did q1a

--- Lab05/code/Lab05_Q1_a.py
+++ Lab05/code/Lab05_Q1_a.py
@@ -9,14 +9,14 @@ Created on Tue Oct  9 16:25:26 2018
 import numpy as np
 import matplotlib.pyplot as plt
 
-month, spots = np.loadtxt('sunspots.txt', unpack=True)
+month, spots = np.loadtxt('../data/sunspots.txt', unpack=True)
 
 plt.figure(0)
 plt.plot(month, spots, '.')
 plt.xlabel('Time (months after 1759)')
 plt.ylabel('Number of sunspots')
 plt.title('Sunspots over time')
-plt.savefig('1a_timeseries.png')
+plt.savefig('../images/1a_timeseries.png')
 
 plt.figure(1)
 plt.plot(month, spots, '.')
@@ -24,7 +24,7 @@ plt.xlabel('Time (months after 1759)')
 plt.ylabel('Number of sunspots')
 plt.title('Sunspots over time (zoomed in)')
 plt.xlim([2500,3000])
-plt.savefig('1a_zoomed_timeseries.png')
+plt.savefig('../images/1a_timeseries_zoomed.png')
 
 spots_fft = np.fft.rfft(spots)
 k = np.arange(0,len(spots_fft))
@@ -34,11 +34,13 @@ plt.plot(k, np.abs(spots_fft)**2)
 plt.ylabel('$|c_k|^2$')
 plt.xlabel('Fourier coefficient numer (k)')
 plt.title('Fourier transform of sunspot data')
+plt.savefig('../images/1a_fourier.png')
 
 plt.figure(3)
 plt.plot(k, np.abs(spots_fft)**2)
-plt.xlim([0,100])
-plt.ylim([0.0,1e10])
+plt.xlim([0,50])
+plt.ylim([0.0,5e9])
 plt.ylabel('$|c_k|^2$')
 plt.xlabel('Fourier coefficient numer (k)')
 plt.title('Fourier transform of sunspot data (zoomed in)')
+plt.savefig('../images/1a_fourier_zoomed.png')
