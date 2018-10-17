ae81ea4cca9f53738d2abeae39d80a4c25239050
pzindel <pierino.zindel@mail.utoronto.ca>
2018-10-16 20:26:16 -0400

    q2 working but not correct

--- Lab06/code/Lab06_Q2.py
+++ Lab06/code/Lab06_Q2.py
@@ -1,130 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 16 12:54:17 2018
+Created on Tue Oct 16 18:37:32 2018
+
 @author: Pierino
 """
-
 #import libraries
 import numpy as np
 import pylab as plt
 
-#function of equations of motion
-def func(r, v, t):
-    #unpack initial conditions
-    x, y = r
-    vx, vy = v
+def func(A, B, t):
+    """
+    ODE function that computes the force on A due to B
+    """
+    #unpack coordinates of A & B into components 
+    x1, y1, vx1, vy1 = A
+    x2, y2, vx2, vy2 = B
     
-    #constants
+    #compute relative coordinates
+    x = (x2 - x1)
+    y = (y2 - y1)
+    
+    #function constants
     epsilon = 1.0
-    m = 1.0
     sigma = 1.0
+    mass = 1.0
+
+    #compute new values
+    Dx = vx1
+    Dy = vy2
     
-    #compute new conditions
     R = (x**2 + y**2)
-    c1 = (64.0 * epsilon * R**(-9) * sigma**(12))/m
-    c2 = (-24.0* epsilon * R**(-4) * sigma**(6))/m
+    c1 = (64.0 * epsilon * R**(-9) * sigma**(12))/mass
+    c2 = (-24.0* epsilon * R**(-4) * sigma**(6))/mass
     
-    Dvx = (c1+c2)*x/2
-    Dvy = (c1+c2)*y/2
-    Dx = vx
-    Dy = vy
+    Dvx = (c1 + c2)*x + x1
+    Dvy = (c1 + c2)*y + y1
     
     return np.array([Dx,Dy,Dvx,Dvy])
 
-#define RK4 method function
-#def RK4(h, f, r, t):
-#    k1 = h*f(r,t)
-#    k2 = h*f(r + k1/2, t + h/2)
-#    k3 = h*f(r + k2/2, t + h/2)
-#    k4 = h*f(r + k3, t + h)    
-#    return r + (k1 + 2*k2 + 2*k3 + k4)/6
-
-
-#def verlet():
-#    v(t+h/2)=v(t) +h*f(r(t),t)/2
-#    
-#    #repeat
-#    r(t+h)=r(t) +h*v(t+h/2)
-#    k = h*f(r(t+h), t+h)
-#    v(t+h)=v(t+h/2)+k/2
-#    v(t+3*h/2)=v(t+h/2)+k
+def plot(r1, r2, path, save):
+    """
+    Function that takes two 2D arrays with format [x,y,vx,vy] and plots the
+    data sets on same graph.
+    """
+    #unpack data sets
+    x1 = r1[:,0]
+    y1 = r1[:,1]
+    x2 = r2[:,0]
+    y2 = r2[:,1]
     
-def solve(r1, r2, f, h, tpoints):
-    N = int(len(tpoints)/2)
-    s1 = np.zeros([N, 2])
-    s2 = np.zeros([N, 2])
-    v1 = np.zeros([N, 2])
-    v2 = np.zeros([N, 2])
-    
-    s1[0][0] = r1[0]
-    s1[0][1] = r1[1]
-    s2[0][0] = r2[0]
-    s2[0][1] = r2[1]
-    v1[0][0] = r1[2]
-    v1[0][1] = r2[3]
-    v2[0][0] = r2[2]
-    v2[0][1] = r2[3]
-    
-    v1[1] = v1[0] + h*f(s1[0], v1[0], 0)[2:]/2
-    v2[1] = v2[0] + h*f(s2[0], v2[0], 0)[2:]/2
-    
-    M = int(len(tpoints-4)/2)
-    
-    for i in range(M):
-        
-        t = i*2
-        
-        r = s1[t+2] - s2[t+2]
-        s1[t+2] = s1[t] + h*v1[t+1]
-        k = h*f(r, v1[t+2], tpoints[t]+h)[2:]
-        v1[t+2] = v1[t+1] + k/2
-        v1[t+3] = v1[t+1] + k
-        
-        r = s2[t+2] - s1[t+2]
-        s2[t+2] = s2[t] + h*v2[t+1]
-        k = h*f(r, v2[t+2], tpoints[t]+h)[2:]
-        v2[t+2] = v2[t+1] + k/2
-        v2[t+3] = v2[t+1] + k
-        
-    return s1, s2
-
-#initial conditions of each part r*=[[i],[ii],[iii]] ,[i]=[x,y] 
-r1_all = np.array([[4.0, 4.0],[4.5, 4.0],[2.0, 3.0]])
-r2_all = np.array([[5.2, 4.0],[5.2, 4.0],[3.5, 4.4]])
-
-#ode solve parameters
-a = 0.0
-b = 1.0
-N = 100
-h = (b-a)/N
-
-#solve and plot for each part
-for i in range(len(r1_all)):
-    #initial conditions & containers
-    x1 = r1_all[i,0]
-    y1 = r1_all[i,1]
-    x2 = r2_all[i,0]
-    y2 = r2_all[i,1]
-    vx1 = 0.0
-    vy1 = 0.0
-    vx2 = 0.0
-    vy2 = 0.0
-    
-    r1 = np.array([x1, y1, vx1, vy1])
-    r2 = np.array([x2, y2, vx2, vy2])
-
-    tpoints = np.arange(a,b,0.5*h)
-    
-    #solve ode
-    s1, s2 = solve(r1, r2, func, h, tpoints)
-
-    #extract x&y values from solution container
-    x1 = s1[:,0]
-    y1 = s1[:,1]
-    x2 = s2[:,0]
-    y2 = s2[:,1]
-    
-    #plot resulting function
+    #plot both molecules
     plt.figure()
     plt.plot(x1, y1, ".", label="1")
     plt.plot(x2, y2, ".", label="2")
@@ -133,8 +58,84 @@ for i in range(len(r1_all)):
     plt.title("Trajectories of molecules")
     plt.grid()
     plt.legend()
-    path  = "../images/q2_b_" + "i"*i +".png"
-    plt.savefig(path, dpi=600)
+    if save:
+        plt.savefig(path, dpi=600)
+        
+def verlet(r1_initial ,r2_initial, f, h, tpoints):
+    """
+    Function that takes two 1D arrays, of the form [x,y,vx,vy], corresponding
+    to the initial conditions of particles 1 and 2. Computes the integral
+    of f using verlet method and returns two 2D arrays containting a set 
+    [x, y, vx, vy] for each point.  
+    """
+    #set containers
+    N = len(tpoints)
+    r1 = np.zeros([N, 4])
+    r2 = np.zeros([N, 4])
+    
+    #set initial conditions
+    r1[0] = r1_initial
+    r2[0] = r2_initial
+    
+    #compute first set
+    r1[1,2:] = r1[0,2:] + h*f(r1[0], r2[0], t=0)[2:]/2
+    r2[1,2:] = r2[0,2:] + h*f(r2[0], r1[0], t=0)[2:]/2
+    
+    
+    # t=i => t + (h*n)/2=i+n  [t_step=0.005; h=0.01]
+    
+    for i in range(len(tpoints)//2-1):
+        i = i*2
+        r1[i+2,:2] = r1[i,:2] + h*r1[i+1,2:]
+        k = h*f(r1[i+2], r2[i+2], tpoints[i+2])[2:]
+        r1[i+2,2:] = r1[i+1,2:] + k/2
+        r1[i+3,2:] = r1[i+1,2:] + k
+        
+        r2[i+2,:2] = r2[i,:2] + h*r2[i+1,2:]
+        k = h*f(r2[i+2], r1[i+2], tpoints[i+2])[2:]
+        r2[i+2,2:] = r2[i+1,2:] + k/2
+        r2[i+3,2:] = r2[i+1,2:] + k
+    
+    s1 = []
+    s2 = []    
+    
+    for i in range(len(r2)):
+        if i%2 == 0:
+           s1.append(r1[i])
+           s2.append(r2[i])
+    
+    return np.array(s1), np.array(s2)    
+        
 
+#integration parameters
+a = 0.0
+b = 1.0
+N = 100
+h = (b-a)/N
+tpoints = np.arange(a, b, 0.5*h)
+
+#save images
+save = False
+
+#I
+r1 = [4.0, 4.0, 0.0, 0.0]
+r2 = [5.2, 4.0, 0.0, 0.0]
+s1, s2 = verlet(r1, r2, func, h, tpoints)
+path = "../images/q2_i.png"
+plot(s1, s2, path, save)
+
+#II
+r1 = [4.5, 4.0, 0.0, 0.0]
+r2 = [5.2, 4.0, 0.0, 0.0]
+s1, s2 = verlet(r1, r2, func, h, tpoints)
+path = "../images/q2_ii.png"
+plot(s1, s2, path, save)
+
+#III
+r1 = [2.0, 3.0, 0.0, 0.0]
+r2 = [3.5, 4.4, 0.0, 0.0]
+s1, s2 = verlet(r1, r2, func, h, tpoints)
+path = "../images/q2_iii.png"
+plot(s1, s2, path, save)
 
 
