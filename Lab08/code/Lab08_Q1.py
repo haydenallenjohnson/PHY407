e80a522980bef414a595796b679a52466cc2e45e
Hayden Allen Johnson <hayden.johnson@mail.utoronto.ca>
2018-10-31 14:59:55 -0400

    q1 stuff

--- Lab08/code/Lab08_Q1.py
+++ Lab08/code/Lab08_Q1.py
@@ -16,9 +16,11 @@ dx = 0.1
 
 #specify target accuracy
 target = 1e-6
+w = 0.9
 
 #create array to hold values
 temp = np.zeros((int(height/dx)+1,int(width/dx)+1),float)
+
 
 #specify boundary conditions of array
 temp[-1,:51] = np.linspace(0,5,51)
@@ -28,34 +30,30 @@ temp[50:,150] = np.linspace(7,5,31)
 temp[-1,150:] = np.linspace(5,0,51)
 temp[:,-1] = np.linspace(10,0,81)
 temp[0,:] = 10
-temp[:,0] - np.linspace(10,0,81)
+temp[:,0] = np.linspace(10,0,81)
 
-'''
-# Main loop
-delta = 1.0
-while delta>target:
-
+#
+counter = 0
+while counter < 100:
+    counter += 1
     # Calculate new values of the potential
-    for i in range(M+1):
-        for j in range(M+1):
-            if i==0 or i==M or j==0 or j==M:
-                phiprime[i,j] = phi[i,j]
-            elif j==M//2 and i>=M//4:
-                phiprime[i,j] = phi[i,j]
+    for i in range(temp.shape[0]):
+        for j in range(temp.shape[1]):
+            if i==0 or i==temp.shape[0]-1 or j==0 or j==temp.shape[1]-1:
+                continue
+            elif 50<=j<=150 and i>=50:
+                continue
             else:
-                phiprime[i,j] = (phi[i+1,j] + phi[i-1,j] \
-                                 + phi[i,j+1] + phi[i,j-1])/4
+                temp[i,j] = ((1+w)/4)*(temp[i+1,j] + temp[i-1,j] + temp[i,j+1] + temp[i,j-1]) - w*temp[i,j]
 
     # Calculate maximum difference from old values
-    delta = max(abs(phi-phiprime))
-
-    # Swap the two arrays around
-    phi,phiprime = phiprime,phi
+#    delta = max(abs(phi-phiprime))
+    pl.clf()
+    pl.imshow(temp)
+    pl.gray()
+    pl.pause(0.01)
+    pl.show()
 
 # Make a plot
-imshow(phi)
-gray()
-show()
 
-print(phi[M//4,M//4])
-'''
+
