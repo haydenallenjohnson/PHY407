1e7a051b3af21e137f3c180a87a331d1d1aa9b2f
pzindel <pierino.zindel@mail.utoronto.ca>
2018-11-02 15:14:28 -0400

    completed q2

--- Lab08/code/Lab08_Q2.py
+++ Lab08/code/Lab08_Q2.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 31 14:25:26 2018
+Solves the wave equation corresponding to the problem of a piano hammer
+striking a piano string. Implements the FTCS method and plots the displacement
+over time.
 
-@author: Pierino
+Created on Wed Oct 31 14:25:26 2018
+@author: Pierino Zindel
 """
 
 #import libraries
 import numpy as np
 import pylab as plt
-
 
 #constant of pde
 v = 100 #m/s
@@ -23,51 +25,79 @@ def psi0(x):
     return C*x*(L-x)*np.exp(-(x-d)**2/(2*sigma**2))/L**2
 
 #grid points
-N = 500
+N = 100
 #grid spacing
 a = L/N
 
+#create arrays containing the initial conditions
 x = np.linspace(0,L,N+1)
 psi = psi0(x)
 phi = np.zeros(N+1, float)
-psi_step = psi
-phi_step = phi
+psi_step = np.copy(psi)
+phi_step = np.copy(phi)
 
-#loop
+#loop time
 t = 0.0
-t_f = 50e-3/100
+t_f = 0.101
 
 #array of positions corresponding to each loop over time
 traj = [phi]
 
-#image
+#array of times corresponding to the list of trajectories
+times = [t]
 
 #frame count
 frame = 0
 
+#images save count
+save = 0
+
+#loop over time
 while t < t_f:
-    #for i in range(1,N):
+    #constant of integration
     c = h*v**2/a**2
+    #FTCS stepping
     phi_step[1:N] = phi[1:N] + h*psi[1:N]
     psi_step[1:N] = psi[1:N] + c*(phi[2:N+1] + phi[0:N-1] - 2*phi[1:N])
     
-    phi = phi_step
-    psi = psi_step
-    traj.append(phi_step)
+    #set old array to the new displacement
+    phi = np.copy(phi_step)
+    psi = np.copy(psi_step)    
+    
+    #increase time
     t += h
     
+    #add current plot to container
+    traj.append(np.copy(phi_step))
+    times.append(t)
+    
+    #increase frame count for 'animation'
     frame += 1
-    if (frame % 50) == 0:
+    
+    #plot frame
+    if (frame % 500) == 0:
         frame = 0
-        plt.clf()
+        #plt.clf()
+        plt.figure()
         plt.plot(x, phi)
         plt.xlim([0,1])
         plt.ylim([-0.0005, 0.0005])
+        plt.xlabel("Position, $x$")
+        plt.ylabel("Displacement, $\phi(x)$")
+        plt.title("Piano string displacement at t=%.5f" %t)
+        plt.grid()
+        
+        #save the plot for certain times
+        if t > 0.1 and save == 2:
+            plt.savefig("q2_t=100ms.png", dpi=600)
+            save += 1
+        elif t > 0.05 and save == 1:
+            plt.savefig("q2_t=50ms.png", dpi=600)
+            save += 1
+        elif t > 0.005 and save == 0:
+            plt.savefig("q2_t=2ms.png", dpi=600)
+            save += 1
+        
         plt.draw()
         plt.pause(0.01)
-    
-#plt.plot(x, phi)
-plt.xlabel("x")
-plt.ylabel("phi")
-plt.show()
 
