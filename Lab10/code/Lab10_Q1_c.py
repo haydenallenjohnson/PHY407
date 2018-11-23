12bd1ee0dc4b67c3f7770365210808c554941619
pzindel <pierino.zindel@mail.utoronto.ca>
2018-11-23 14:05:43 -0500

    q1

--- Lab10/code/Lab10_Q1_c.py
+++ Lab10/code/Lab10_Q1_c.py
@@ -41,50 +41,62 @@ def stuck_check(i,j,particles):
     #check for the boundary
     if i  == 0 or j == 0 or i == (L-1) or j == (L-1):
         return True
-    #check for surrounding particles
-    for particle in particles:
-        if i == (particle[0]+1) and j == particle[1]:
-            return True
-        elif i == (particle[0]-1) and j == particle[1]:
-            return True
-        elif i == particle[0] and j == (particle[1]+1):
-            return True
-        elif i == particle[0] and j == (particle[1]-1):
-            return True
+    #return false if no particles are stuck yet
+    if len(particles) < 1:
+        return False
+    if len(particles) > 1:
+        x_fixed = np.array(particles)[:,0]
+        y_fixed = np.array(particles)[:,1]
+        if i-1 in x_fixed:
+            index_list = np.where(x_fixed == i-1)
+            for index in index_list[0]:
+                if j == y_fixed[index]:
+                    return True
+        if i+1 in x_fixed:
+            index_list = np.where(x_fixed == i+1)
+            for index in index_list[0]:
+                if j == y_fixed[index]:
+                    return True
+        if j-1 in y_fixed:
+            index_list = np.where(y_fixed == j-1)
+            for index in index_list[0]:
+                if i == x_fixed[index]:
+                    return True
+        if j+1 in y_fixed:
+            index_list = np.where(y_fixed == j+1)
+            for index in index_list[0]:
+                if i == x_fixed[index]:
+                    return True    
     return False
 
 #function that plots the positions of all stuck particles 
 def plot_traj(traj, save):
     plt.figure()
-    plt.title("2D Brownian Motion Simulation")
+    plt.title("2D Diffusion-Limited Aggregation  Simulation")
     plt.ylabel("y position")
     plt.xlabel("x position")
-    plt.xlim(0,150)
-    plt.ylim(0,150)
-    coord = np.array(traj)
-    if len(traj) > 0: plt.plot(coord[:,0], coord[:,1], '.',c='blue')
+    plt.xlim(0,L-1)
+    plt.ylim(0,L-1)
+    if len(traj) > 0: plt.plot(traj[:,0], traj[:,1], '.',c='blue')
     plt.grid()
     if save:
         plt.savefig("../images/brownian_dla.png", dpi=500)
     plt.show()
     
 #container for positions of all stuck particles
-particles = []
-#container for the trajectories of all particles
-trajectories = []
+particles = np.zeros([2000,2])
 #bool to quit the program when the starting point is stuck
 quit_loop = False
 #seed the random generator
 seed(time())
+#particle counter
+particle_count = 0
 
 #loop over each particle
 while True:
     #set starting position
     i = L//2
     j = L//2
-    #containers for the particles trajectory
-    x = [i]
-    y = [j]
     #tracking variables for terminating the program when done
     stuck = False
     step_count = 0
@@ -94,22 +106,21 @@ while True:
         #move the position
         i,j = move(i,j)
         step_count += 1
-        #store the coordinates
-        x.append(i)
-        y.append(j)
         #check if the particle is done moving
-        stuck = stuck_check(i,j,np.array(particles))
+        stuck = stuck_check(i,j,particles[:particle_count])
         #terminate loop if stuck particles have reached starting position
         if stuck and step_count < 2:
             quit_loop = True
             break
     
     #store the final coordinates of the particle
-    particles.append([i,j])
+    particles[particle_count] = [i,j]
+    particle_count += 1
     #plot position of stuck particles
-    plot_traj(particles, False)
+    plot_traj(particles[:particle_count], False)
     #terminate loop if stuck particles have reach starting position
     if quit_loop: break
 
 #plot final positions of all particles and save the figure
-plot_traj(particles, True)
+plot_traj(particles[:particle_count], True)
+
